114bbaeb27402d5465e4789cd2140d2966952ceb
KrishnaAjay-27 <131335926+KrishnaAjay-27@users.noreply.github.com>
2024-11-08 09:52:58 +0530

    Add files via upload

--- deletesubcategory.php
+++ deletesubcategory.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
+require('connection.php');
 if (!isset($_SESSION['uid'])) {
     header('Location: login.php');
     exit();
 }
 
-$con = mysqli_connect("localhost", "root", "", "project");
+
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
